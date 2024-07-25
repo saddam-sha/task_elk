9037dde4663f74654d7561b988748d8b310b1d8d
saddam_sha <saddmsheikh357@gmail.com>
2024-07-25 09:35:32 +0530

    final commit

---
+++ app/src/main/java/com/example/elk_doc/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.elk_doc;
+
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.VideoView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    VideoView videoView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        videoView= findViewById(R.id.videoview);
+        Uri uri = Uri.parse("android.resource://"+ getPackageName()+"/"+R.raw.v);
+        videoView.setVideoURI(uri);
+        videoView.start();
+
+        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mp.setLooping(true);
+            }
+        });
+
+    }
+    protected void onPostResume(){
+        videoView.resume();
+        super.onPostResume();
+    }
+
+}
